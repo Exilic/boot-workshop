47746769deda51c0d3ebd9722a7ed7203080f1a5
Exilic <tobias.engberg@exilic.se>
2023-01-25 09:28:47 +0100

    Empty project before necessary configurations

---
+++ src/test/java/se/lexicon/bootworkshop/BootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.bootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
